ce76a28d677229ef9cf6bcd1dc90cdeceaa08dda
hakpan <hakpan@umn.edu>
2018-01-13 13:56:56 -0600

    added javascript links, page, and id's to buttons

---
+++ assets/javascript/welcomePage.js
@@ -0,0 +1,19 @@
+//alert("Test");
+$(document).ready(function(){
+
+    $("#mBtn").click(function(){
+        alert("I Work!")
+    });
+
+
+    $("#vBtn").click(function(){
+        alert("I Work!")
+    });
+
+
+    $("#bBtn").click(function(){
+        alert("I Work!")
+    });
+
+
+});
