7fdf77ea59351fade2054cc34b69c58fef5b75ad
Zachary Lockner <zalockner@gmail.com>
2018-01-23 17:22:25 -0600

    added user information to navbar

--- assets/javascript/user.js
+++ assets/javascript/user.js
@@ -27,7 +27,9 @@ firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
 
 firebase.auth().onAuthStateChanged(firebaseUser => {
 	if(firebaseUser) {
-		console.log(firebaseUser);
+		console.log(firebaseUser.email);
+		$("#user-item").text("Log out");
+		$("#menu").append('<li class="nav-item"><a class="nav-link" href="#">'+ firebaseUser.email +'</a></li>');
 	} else {
 		console.log("not logged in");
 	};
