5e294c9117d21b3e36688c7c8e71ee44bd2fde14
Zachary Lockner <zalockner@gmail.com>
2018-01-23 18:43:59 -0600

    adding ability to add venues to database

--- assets/javascript/childadded.js
+++ assets/javascript/childadded.js
@@ -2,13 +2,25 @@
 // Retrieve list of musicians using child_added
 
 dataRef.ref().on("child_added", function(snapshot) {
-  	
-  // Build up musician table in DOM.
-$("#musicianList").append("<tr>" +
-	"<th>" + snapshot.val().username + "</th>" +
-	"<th>" + snapshot.val().url + "</th>" +
-	"<th>" + snapshot.val().city + "</th>" +
-	"<th>" + snapshot.val().state + "</th>" +
-	"<th>" + snapshot.val().genre + "</th>" +
-	"</tr>");
+	var type = snapshot.val().userType;
+
+	if (type == "artist") {
+		  // Build up musician table in DOM.
+		$("#musicianList").append("<tr>" +
+		"<th>" + snapshot.val().username + "</th>" +
+		"<th>" + snapshot.val().url + "</th>" +
+		"<th>" + snapshot.val().city + "</th>" +
+		"<th>" + snapshot.val().state + "</th>" +
+		"<th>" + snapshot.val().genre + "</th>" +
+		"</tr>");
+	} else {
+		$("#venueList").append("<tr>" +
+		"<th>" + snapshot.val().username + "</th>" +
+		"<th>" + snapshot.val().city + " ," + snapshot.val().state + "</th>" +
+		"<th>" + snapshot.val().gigsAvailable + "</th>" +
+		"<th>" + snapshot.val().gigDate + "</th>" +
+		"<th>" + snapshot.val().ggenre + "</th>" +
+		"<th>" + snapshot.val().budget + "</th>" +
+		"</tr>");
+	}
 });
